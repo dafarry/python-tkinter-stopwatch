09f3cc2d24978b596f2da1c44aa4a583e7d90b9b
dafarry <32924849+dafarry@users.noreply.github.com>
2020-03-08 13:40:08 +0000

    Add files via upload

--- python-tkinter-stopwatch.py
+++ python-tkinter-stopwatch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
+from enum import Enum
 import time
 
 class digitalclock(tk.Frame):
@@ -8,28 +9,27 @@ class digitalclock(tk.Frame):
         super().__init__(master, **kwords)
         bframe = tk.Frame(self)
         bframe.pack()
-        buttons = ["Start", "Stop", "Reset",  "Quit"]
-        self.buts = dict(zip(buttons, buttons))
-        for bname in self.buts:
-            bcall = lambda action=self.buts[bname]: self.showtime(action)
-            tk.Button(bframe, text=bname, command=bcall).pack(side="left")
+        self.butn = Enum('butn', "Start Stop Reset Quit")
+        for bname in self.butn:
+            bcall = lambda action=bname: self.showtime(action)
+            tk.Button(bframe, text=bname.name, command=bcall).pack(side="left")
         self.lab = tk.Label(self, text="00:00:00", bg=self.cget("bg"))
         self.lab.pack(side="bottom")
-        self.showtime(self.buts["Reset"])
+        self.showtime(self.butn.Reset)
     def showtime(self, *args):
         if args:
             act = args[0]
-            if act == self.buts["Quit"]:
+            if act == self.butn.Quit:
                 self.run = False
                 self.quit()
-            elif act == self.buts["Reset"]:
+            elif act == self.butn.Reset:
                 self.timer = 0.0
                 self.run = False
                 self.lab.config(text = "00:00:00")
-            elif act == self.buts["Start"] and self.run == False:
+            elif act == self.butn.Start and self.run == False:
                 self.timer = time.time() - self.timer
                 self.run = True
-            elif act == self.buts["Stop"] and self.run == True:
+            elif act == self.butn.Stop and self.run == True:
                 self.timer = time.time() - self.timer
                 self.run = False
         if self.run == True:
